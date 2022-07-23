4c62a2b2ce126bda71cc671e63989b7bec7bade1
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2022-07-23 19:21:09 -0300

    Convert id to integer

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import dotenv
 
 dotenv.load_dotenv()
 BOT_TOKEN = dotenv.get_key(dotenv_path=dotenv.find_dotenv(), key_to_get="BOT_TOKEN")
-BOT_OWNER_ID = dotenv.get_key(
+BOT_OWNER_ID = int(
+    dotenv.get_key(
     dotenv_path=dotenv.find_dotenv(), key_to_get="BOT_OWNER_ID"
+    )
 )
